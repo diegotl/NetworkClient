bec6a2da5ead2f411fce368b85b8682180f9b2ea
Diego Trevisan Lara <diego@trevisa.nl>
2021-02-12 18:15:50 +0100

    DownloadTask.

--- Sources/Client/APIClient.swift
+++ Sources/Client/APIClient.swift
@@ -11,7 +11,7 @@ import Combine
 public class APIClient: APIClientProtocol {
 
     let adapters: [RequestAdapter]
-    
+
     public init(adapters: [RequestAdapter] = []) {
         self.adapters = adapters
     }
@@ -51,5 +51,26 @@ public class APIClient: APIClientProtocol {
             .receive(on: RunLoop.main)
             .eraseToAnyPublisher()
     }
-    
+
+    public func download(apiRequest: APIRequest, destination: URL) -> AnyPublisher<URL, Error> {
+        var request = apiRequest.build()
+        adapters.forEach({ request = $0.adapt(request) })
+
+        return Future { promise in
+            URLSession.shared.downloadTask(with: request) { [weak self] url, response, error in
+                self?.adapters.forEach { $0.complete(request: request, response: response, data: nil) }
+                
+                do {
+                    if let error = error { throw error }
+                    let sourcePath = url?.path ?? ""
+                    try FileManager.default.moveItem(atPath: sourcePath, toPath: destination.path)
+                    promise(.success(destination))
+                }
+                catch {
+                    promise(.failure(error))
+                }
+            }.resume()
+        }.eraseToAnyPublisher()
+    }
+
 }
